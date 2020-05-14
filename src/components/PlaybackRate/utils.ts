0ee23e2cc08c74816ffff864caf13e25fa50184a
dalyIsaac <isaac.daly@outlook.com>
2020-05-14 15:39:34 +1200

    Updated playback rate constants.

--- src/components/PlaybackRate/utils.ts
+++ src/components/PlaybackRate/utils.ts
@@ -1,6 +1,6 @@
-export const PLAYBACKRATE_STEP = 0.2;
+export const PLAYBACKRATE_STEP = 0.1;
 export const PLAYBACKRATE_MIN = 0.4;
-export const PLAYBACKRATE_MAX = 20.0;
+export const PLAYBACKRATE_MAX = 3.0;
 
 export function roundPlaybackRate(value: number) {
   let valueStr = value.toPrecision(2);
