f1b2c785f09d22c2ea7ec8dac3873de520931853
dalyIsaac <dalyIsaac@users.noreply.github.com>
2020-10-20 22:04:58 +0000

    deploy: 49cc6a131dc303c7fc58ea13e7169da94cfa5e15

---
+++ precache-manifest.f2d113a3475aee0c2fa3fda7b30eaee3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88e3d9547b8903a49957cb894342c4d5",
+    "url": "/video-player/index.html"
+  },
+  {
+    "revision": "c6255fe92a86b12a6f13",
+    "url": "/video-player/static/css/main.ee033682.chunk.css"
+  },
+  {
+    "revision": "72ebf65094a02e522d1a",
+    "url": "/video-player/static/js/2.89303a7d.chunk.js"
+  },
+  {
+    "revision": "99aac3a12f53ecda64d8df8cd77c0a23",
+    "url": "/video-player/static/js/2.89303a7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d3a7f8335d2da68dc20",
+    "url": "/video-player/static/js/3.0142e098.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/video-player/static/js/3.0142e098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8813a6f7cbfb2f2cd056",
+    "url": "/video-player/static/js/4.863015c1.chunk.js"
+  },
+  {
+    "revision": "c6255fe92a86b12a6f13",
+    "url": "/video-player/static/js/main.45c87d97.chunk.js"
+  },
+  {
+    "revision": "fdfc3e183bc13118415f",
+    "url": "/video-player/static/js/runtime-main.ee393b2e.js"
+  }
+]);
