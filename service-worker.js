ed827bb5fb5eb1eb9c2d911ed59b6007df361323
dalyIsaac <dalyIsaac@users.noreply.github.com>
2020-11-02 04:01:57 +0000

    deploy: 21a8d4a7faf7e6e830f94463be5dfcde8c3e6c47

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/video-player/precache-manifest.f2d113a3475aee0c2fa3fda7b30eaee3.js"
+  "/video-player/precache-manifest.f6a8f1fc51c372809f21364cf53a1567.js"
 );
 
 self.addEventListener('message', (event) => {
